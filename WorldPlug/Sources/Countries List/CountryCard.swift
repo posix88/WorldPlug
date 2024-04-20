fddf3ec0851c02f344e162b5fe0954889f4c58e3
Antonino Francesco Musolino <antonino.musolino@moneyfarm.com>
2024-04-20 15:17:17 +0200

    small changes

---
+++ WorldPlug/Sources/Countries List/CountryCard.swift
@@ -0,0 +1,87 @@
+//
+//  CountryCard.swift
+//  WorldPlug
+//
+//  Created by Antonino Musolino on 20/04/24.
+//
+
+import SwiftUI
+import Repository_iOS
+
+struct CountryCard: View {
+    let country: Country
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            HStack {
+                Text(country.flagUnicode)
+                    .font(.system(size: 30))
+
+                Text(country.name)
+                    .font(.headline)
+                    .foregroundStyle(WorldPlugAsset.Assets.textRegular.swiftUIColor)
+            }
+            .padding(.bottom, 8)
+
+            HStack {
+                HStack(spacing: 4) {
+                    Image(systemName: "bolt.circle")
+                        .imageScale(.medium)
+
+                    Text(country.voltage)
+                        .font(.caption)
+                }
+                .foregroundStyle(WorldPlugAsset.Assets.volt.swiftUIColor)
+
+                HStack(spacing: 4) {
+                    Image(systemName: "waveform")
+                        .imageScale(.medium)
+
+                    Text(country.frequency)
+                        .font(.caption)
+                }
+                .foregroundStyle(WorldPlugAsset.Assets.frequency.swiftUIColor)
+            }
+            .padding(.bottom, 16)
+
+
+            HStack {
+                ForEach(country.sortedPlugs) { plug in
+                    HStack(spacing: 8) {
+                        Image(systemName: plug.plugSymbol)
+                            .imageScale(.small)
+
+                        Text(plug.id)
+                            .font(.caption2)
+                            .foregroundStyle(WorldPlugAsset.Assets.textRegular.swiftUIColor)
+                    }
+                    .padding(.all, 5)
+                    .background(WorldPlugAsset.Assets.surfaceSecondary.swiftUIColor)
+                    .roundedCornerWithBorder(radius: 8, lineWidth: 1)
+                }
+            }
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .embedInCard()
+    }
+}
+
+#if DEBUG
+import SwiftData
+
+#Preview {
+    let config = ModelConfiguration(isStoredInMemoryOnly: true)
+    let container = try! ModelContainer(for: Country.self, configurations: config)
+    let country = Country(code: "IT", voltage: "230V", frequency: "50Hz", flagUnicode: "🏴‍☠️")
+    container.mainContext.insert(country)
+    country.plugs = [
+        Plug(id: "A", name: "Type A", info: "info", images: []),
+        Plug(id: "B", name: "Type B", info: "info", images: []),
+        Plug(id: "C", name: "Type B", info: "info", images: []),
+        Plug(id: "D", name: "Type B", info: "info", images: [])
+    ]
+
+    return CountryCard(country: country)
+        .padding()
+}
+#endif
